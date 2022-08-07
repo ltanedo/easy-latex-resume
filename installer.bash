e0c08b4b57bb5a081c39c53e9b414aed6c46e9d9
ltanedo <lloydtan@buffalo.edu>
2022-08-07 13:26:48 -0400

    init

---
+++ installer.bash
@@ -0,0 +1,10 @@
+#/bin/sh
+
+brew install --cask mactex-no-gui
+ext install latex-workshop
+
+source ~/.bashrc 
+source ~/.zsshrc 
+
+# "latex-workshop.latex.recipe.default": "latexmk (lualatex)",
+# ~/Library/Application Support/Code/User/settings.json
